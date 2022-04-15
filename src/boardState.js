4c3a77a6e08730ddd26501bfcadc31f4667535ba
Malakai Johnson <malakai.johnson642@gmail.com>
2022-04-14 17:40:47 -0700

    removed Player objects because nested objects are bad for Firebase

--- src/boardState.js
+++ src/boardState.js
@@ -13,7 +13,7 @@ import {
 
 export class BoardState
 {
-  constructor (round = 0, players = new PlayerStates())
+  constructor (round = 0, players = new Array({x: 0, y: 0, direction: 'north'}))
   {
     this.round = round;
     this.players = players;
@@ -24,23 +24,22 @@ export class BoardState
     // output = output + "Player 0: " + JSON.stringify(this.players[0]) + "\n";
     for(let i = 0; i < this.players.length; i++)
     {
-      output = output + this.players.toString();
+      output = output + "Player " + i + ": " + JSON.stringify(this.players[i]) + "\n";
     }
     return output;
-  }
 }
 
 export const boardStateConverter = {
   toFirestore: (boardState) => {
     return {
       round: boardState.round,
-      players: boardState.players.withConverter(playerStatesConverter),
+      players: boardState.players,
     };
   },
   fromFirestore: (snapshot, options) =>
   {
     const data = snapshot.data(options);
-    return new BoardState(data.round, data.players.withConverter(playerStatesConverter));
+    return new BoardState(data.round, data.players);
   }
 }
 
@@ -56,77 +55,5 @@ export async function getBoardState(database, gameid)
   {
     console.log("No boardState!");
     return null;
-  }
-}
-
-export class PlayerStates
-{
-  constructor (players = new Array(new Player()))
-  {
-    this.players = players;
-  }
-
-  toString()
-  {
-    outputString = "";
-    this.players.forEach((player, i) => {
-      outputString = outputString + "Player " + i + ": " + player.toString() + "\n";
-    });
-    return outputString;
-  }
-
-  addPlayerState(newPlayer = new Player())
-  {
-    this.players.push(newPlayer);
-  }
-}
-
-export const playerStatesConverter = {
-  toFirestore: (playerStates) => {
-    const playerStatesArray = newArray();
-    playerStates.forEach((player, i) => {
-      playerStatesArray.push(player.withConverter(playerConverter));
-    });
-
-    return {
-      players: playerStatesArray,
-    };
-  },
-  fromFirestore: (snapshot, options) =>
-  {
-    const data = snapshot.data(options);
-    return new PlayerStates(data.players);
-  }
-}
-
-export class Player
-{
-  constructor (position = [0, 0], direction = "north", isReady = false, nextProgram = null)
-  {
-    this.position = position;
-    this.direction = direction;
-    this.isReady = isReady;
-    this.nextProgram = nextProgram;
-  }
-
-  toString()
-  {
-    return "Position: " + this.position + ", Direction: " + direction + ") Ready: " + isReady;
-  }
-}
-
-export const playerConverter = {
-  toFirestore: (player) => {
-    return {
-      position: player.postition,
-      direction: player.direction,
-      ready: player.ready,
-      nextProgram: player.nextProgram,
-    };
-  },
-  fromFirestore: (snapshot, options) =>
-  {
-    const data = snapshot.data(options);
-    return new Player(data.position, data.direction, data.ready, data.nextProgram);
   }
 }
